1885178e3d5f82b21e185e54df56de10ad426580
Correy Lim <correylim@gmail.com>
2021-07-14 15:12:20 -0700

    Commit build directory for gh-pages compat
    Ensures GitHub pages functionality works

---
+++ build/precache-manifest.5ba1ad5463dd9a610d6b6b6a2e892b65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2fd259ea74aca875e6a5",
+    "url": "/webviewer-react-sample/static/css/main.ec60d9b0.chunk.css"
+  },
+  {
+    "revision": "2fd259ea74aca875e6a5",
+    "url": "/webviewer-react-sample/static/js/main.2fd259ea.chunk.js"
+  },
+  {
+    "revision": "78d43f1a99c80cd94a28",
+    "url": "/webviewer-react-sample/static/js/1.78d43f1a.chunk.js"
+  },
+  {
+    "revision": "23dd03fbedbe42b205ef",
+    "url": "/webviewer-react-sample/static/js/runtime~main.23dd03fb.js"
+  },
+  {
+    "revision": "b29fda0fef310564dd3a44b2a54038a9",
+    "url": "/webviewer-react-sample/index.html"
+  }
+];
